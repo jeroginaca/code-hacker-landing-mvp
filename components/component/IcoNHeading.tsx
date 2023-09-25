31b723231e9723294d34da4fe53474dee77f5aaa
JeronimoGinaca <jeronimo.ginaca@globant.com>
2023-09-25 13:52:08 +0200

    nav-bg-asdf

--- components/component/IcoNHeading.tsx
+++ components/component/IcoNHeading.tsx
@@ -7,3 +7,56 @@ const IcoNHeading = () => {
 }
 
 export default IcoNHeading
+
+
+
+// "use client"
+
+// import Image from 'next/image'
+// import Link from 'next/link'
+// import { NavLinks, NavLinksResp } from '.'
+// import { useEffect, useState } from 'react';
+
+// const Navbar = () => {
+//   const [scrolling, setScrolling] = useState(false);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (window.scrollY > 0) {
+//         setScrolling(true);
+//       } else {
+//         setScrolling(false);
+//       }
+//     };
+
+//     window.addEventListener('scroll', handleScroll);
+
+//     return () => {
+//       window.removeEventListener('scroll', handleScroll);
+//     };
+//   }, []);
+
+//   return (
+//     <>
+//       <div className={`navbar flex justify-between items-center  text-white fixed w-[100%] pl-[0.5rem] pr-[1.7rem] md:px-[3rem] pt-[7px] z-[10000] ${scrolling ? 'gradient-bg' : ''}`}>
+//         <Link href="/">
+//           <Image 
+//             src="https://res.cloudinary.com/dr3cvyonp/image/upload/v1694439448/Logo_qbbelb.png"
+//             width={95}
+//             height={95}
+//             alt="logo"
+//           />
+//         </Link>
+
+//         <NavLinks />
+
+//         <Link href="#contact"><button className="hidden md:flex text-[20px]  py-[12px] px-[30px] font-bold tracking-[.10em] rounded-[15px] bg-transparent">Contacto</button></Link> 
+         
+//         <NavLinksResp />
+//       </div>
+
+//     </>
+//   )
+// }
+
+// export default Navbar
