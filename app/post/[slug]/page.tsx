e261d2a778544d4994144c73fa693a4958490a47
JeronimoGinaca <jeronimo.ginaca@globant.com>
2024-01-10 21:55:11 +0100

    blog y landing bootcamp5

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -24,7 +24,7 @@ export default async function SlugPage({
       image: ({ value }: { value: any }) => (
         <Image
           src={urlFor(value).url()}
-          alt="Image"
+          alt="Image alt"
           className="rounded-lg"
           width={800}
           height={800}
